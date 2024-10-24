f3d9b3b4977b8a7cfee00792c033151d22e673cc
Rd_Haunted <rohitdey1204@gmail.com>
2024-10-24 14:00:51 -0500

    header info

--- src/Sheard/Header/Header.jsx
+++ src/Sheard/Header/Header.jsx
@@ -1,7 +1,36 @@
+import { Box, Container, styled, } from '@mui/material'
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import assests from '../../assests'
+const Headerwraperstyle = styled(Box)`
+.MuiContainer-root {
+    max-width: 1446px;
+    margin: auto;
+    padding: 0px 12px;
+}
+`
 export default function Header() {
   return (
-    <div>Header</div>
+    <Headerwraperstyle>
+        <Container>
+        <Box className='header_outer'>
+            <Box className='header_outer_inner_left'>
+                <Link to='/'> 
+                <figure>
+                    <img src={assests.logo_image} alt="this is a image" />
+                </figure>
+                </Link>
+            </Box>
+            <Box className='header_outer_inner_right'>
+                <Box className='header_outer_inner_right_top'>
+                    
+                </Box>
+                <Box className='header_outer_inner_right_bottom'>
+
+                </Box>
+            </Box>
+        </Box>
+        </Container>
+    </Headerwraperstyle>
   )
 }
--- src/assests/index.js
+++ src/assests/index.js
@@ -1,3 +1,3 @@
 export default{
-    
+logo_image:"/assests/images/tree_logo.png",
 }
